d63095dd4c9196b80ecd3ba1d3dd6ccd7d2e1729
Nowakowski <mjnowakowski@labs.neit.edu>
2019-03-09 16:58:01 -0500

    SaveCommit

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const config = require("./config.js");
 const Server = require("./lib/server/server.js");
 
-let server = new Server(config.server.listenPort, config.keyGenOptions);
+let server = new Server(config.server);
 server.startServer();
-
-const PuzzleLoader = require("./lib/game/board/puzzleloader.js");
-let pl = new PuzzleLoader();
-let board = pl.loadPuzzle("puzzle1");
--- config.js
+++ config.js
@@ -1,19 +1,6 @@
 const config = {
 	server: {
 		listenPort: process.env.RUSHHOUR_LISTENPORT || 80
-	},
-	keyGenOptions: {
-		length:25,
-		charRanges: [
-			{
-				low:48,
-				high:57
-			},
-			{
-				low:97,
-				high:122
-			}
-		]
 	}
 }
 
