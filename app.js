cb2c5fe57be8ea86b33e736c7b7e7272e0709aad
Nowakowski <mjnowakowski@labs.neit.edu>
2019-03-09 16:20:50 -0500

    Restructured Loaded puzzle to memory

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const config = require("./config.js");
-const Server = require("./lib/server.js");
+const Server = require("./lib/server/server.js");
 
 let server = new Server(config.server.listenPort, config.keyGenOptions);
 server.startServer();
+
+const PuzzleLoader = require("./lib/game/board/puzzleloader.js");
+let pl = new PuzzleLoader();
+let board = pl.loadPuzzle("puzzle1");
